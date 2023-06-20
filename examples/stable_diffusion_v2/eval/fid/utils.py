7bd5ed8a0bc0b1bd18bde8486d7a6cc36efb1a89
Samit <285365963@qq.com>
2023-06-20 23:46:34 +0800

    add fid eval

--- examples/stable_diffusion_v2/eval/fid/utils.py
+++ examples/stable_diffusion_v2/eval/fid/utils.py
@@ -1,4 +1,7 @@
-"""Utility of downloading"""
+"""Utility"""
+from typing import Callable, Dict, Optional
+import numpy as np
+from PIL import Image
 import bz2
 import gzip
 import hashlib
@@ -12,16 +15,17 @@ import urllib.request
 import zipfile
 from copy import deepcopy
 from typing import Optional
-
 from tqdm import tqdm
-
 import pathlib
+from mindspore import load_checkpoint, load_param_into_net
 
 _logger = logging.getLogger(__name__)
 # The default root directory where we save downloaded files.
 # Use Get/Set to R/W this variable.
 _DEFAULT_DOWNLOAD_ROOT = os.path.join(os.path.expanduser("~"), ".mindspore")
 
+IMAGE_EXTENSIONS = {'bmp', 'jpg', 'jpeg', 'pgm', 'png', 'ppm',
+                    'tif', 'tiff', 'webp', 'JPEG'}
 
 def get_default_download_root():
     return deepcopy(_DEFAULT_DOWNLOAD_ROOT)
@@ -31,6 +35,9 @@ def set_default_download_root(path):
     global _DEFAULT_DOWNLOAD_ROOT
     _DEFAULT_DOWNLOAD_ROOT = path
 
+
+def get_checkpoint_download_root():
+    return os.path.join(get_default_download_root(), "models")
 
 FILE_TYPE_ALIASES = {
     ".tbz": (".tar", ".bz2"),
@@ -74,7 +81,7 @@ def detect_file_type(filename: str):  # pylint: disable=inconsistent-return-stat
                 return suffix2 + suffix, suffix2, suffix
         return suffix, None, suffix
 
-class DownLoad:
+class Download:
     """Base utility class for downloading."""
 
     USER_AGENT: str = (
@@ -140,13 +147,19 @@ class DownLoad:
 
     def download_file(self, url: str, file_path: str, chunk_size: int = 1024):
         """Download a file."""
+
+        # no check certificate
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
         # Define request headers.
         headers = {"User-Agent": self.USER_AGENT}
 
         _logger.info(f"Downloading from {url} to {file_path} ...")
         with open(file_path, "wb") as f:
             request = urllib.request.Request(url, headers=headers)
-            with urllib.request.urlopen(request) as response:
+            with urllib.request.urlopen(request, context=ctx) as response:
                 with tqdm(total=response.length, unit="B") as pbar:
                     for chunk in iter(lambda: response.read(chunk_size), b""):
                         if not chunk:
@@ -160,7 +173,7 @@ class DownLoad:
         path: Optional[str] = None,
         filename: Optional[str] = None,
         md5: Optional[str] = None,
-    ) -> None:
+    ):
         """Download a file from a url and place it in root."""
         if path is None:
             path = get_default_download_root()
@@ -175,7 +188,7 @@ class DownLoad:
         # Check if the file is exists.
         if os.path.isfile(file_path):
             if not md5 or self.check_md5(file_path, md5):
-                return
+                return file_path
 
         # Download the file.
         try:
@@ -192,6 +205,8 @@ class DownLoad:
                     ssl._create_default_https_context = ssl.create_default_context
             else:
                 raise e
+
+        return file_path
 
     def download_and_extract_archive(
         self,
@@ -217,3 +232,84 @@ class DownLoad:
 
         if remove_finished:
             os.remove(archive)
+
+
+def download_model(url):
+    """Download the pretrained ckpt from url to local path"""
+
+    # download files
+    download_path = get_checkpoint_download_root()
+    os.makedirs(download_path, exist_ok=True)
+    file_path = Download().download_url(url, path=download_path)
+    return file_path
+
+
+def load_model(
+    network,
+    load_from: Optional[str] = None,
+    filter_fn: Optional[Callable[[Dict], Dict]] = None,
+):
+    """
+    Load the checkpoint into the model
+
+    Args:
+        network: network
+        load_from: a string that can be url or local path to a checkpoint, that will be loaded to the network.
+        filter_fn: a function filtering the parameters that will be loading into the network. If it is None,
+            all parameters will be loaded.
+    """
+    if load_from is None:
+        return
+
+    if load_from[:4] == "http":
+        local_ckpt_path = download_model(load_from)
+    else:
+        local_ckpt_path = load_from
+
+    assert local_ckpt_path and os.path.exists(local_ckpt_path), (
+        f"Failed to load checkpoint. `{local_ckpt_path}` NOT exist. \n"
+        "Please check the path and set it in `eval-ckpt_load_path` or `model-pretrained` in the yaml config file "
+    )
+
+    params = load_checkpoint(local_ckpt_path)
+
+    if filter_fn is not None:
+        params = filter_fn(params)
+
+    load_param_into_net(network, params)
+
+
+def get_image_paths(img_dir):
+    path = pathlib.Path(img_dir)
+    files = sorted([file for ext in IMAGE_EXTENSIONS
+                    for file in path.glob('*.{}'.format(ext))])
+
+    return files
+
+def compute_torchmetric_fid(gen_imgs, gt_imgs):
+    import torch
+    import torchmetrics as tm
+    from torchvision.transforms import functional as F
+
+    real_images = [np.array(Image.open(path).convert("RGB")) for path in gt_imgs]
+    fake_images = [np.array(Image.open(path).convert("RGB")) for path in gen_imgs]
+
+    def preprocess_image(image):
+        image = torch.tensor(image).unsqueeze(0)
+        image = image.permute(0, 3, 1, 2) / 255.0
+        return image
+
+    real_images = torch.cat([preprocess_image(image) for image in real_images])
+    #print(real_images.shape)
+    fake_images = torch.cat([preprocess_image(image) for image in fake_images])
+    #print(fake_images.shape)
+
+    # torch
+    fid = tm.image.fid.FrechetInceptionDistance(normalize=True)
+
+    fid.update(real_images, real=True)
+    fid.update(fake_images, real=False)
+
+    return float(fid.compute())
+
+
