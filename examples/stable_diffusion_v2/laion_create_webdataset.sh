bab4910a19b9fb4cb1a542882d6e641449200ed6
Samit <285365963@qq.com>
2023-08-04 10:45:17 +0800

    tmp

--- examples/stable_diffusion_v2/laion_create_webdataset.sh
+++ examples/stable_diffusion_v2/laion_create_webdataset.sh
@@ -1,9 +1,9 @@
 #data_dir='/Volumes/Extreme_SSD/LAION/2b_en_ae4.5_filtered/part_1'
-data_dir="/Volumes/LIN RUI/laion2b_en/sd2.1_base_train/text_image_data/part_3"
+data_dir="/Volumes/LIN RUI/laion2b_en/sd2.1_base_train/image_text_data/part_3"
 
 cd $data_dir
 #for i in {00000..00127}
-for i in {00000..00015}; do
+for i in {00007..00015}; do
    echo $i
    gtar --sort=name -cf $i.tar $i
    rm -rf $i
