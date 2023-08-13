b4e0fe8ef9ee805cae1dfaac8ed7702b7a8e8c0a
Samit <285365963@qq.com>
2023-08-13 14:08:32 +0800

    verify allocate tars

--- examples/stable_diffusion_v2/tools/data_utils/allocate_tars.py
+++ examples/stable_diffusion_v2/tools/data_utils/allocate_tars.py
@@ -15,9 +15,8 @@ def get_split(num_samples, num_devices, device_id):
     start_tar_idx = -1
     start_sample_idx = -1
 
-    cur_device_id = 0
-    start_global_sample_idx = samples_per_device * cur_device_id
-    end_global_sample_idx = samples_per_device * (cur_device_id+1) - 1
+    start_global_sample_idx = samples_per_device * device_id
+    end_global_sample_idx = samples_per_device * (device_id+1) - 1
     #print(start_global_sample_idx, end_global_sample_idx)
 
     p1 = 0 # pointer 1 towards allocation segment head
@@ -29,24 +28,40 @@ def get_split(num_samples, num_devices, device_id):
                 start_part_idx = j
                 start_tar_idx = k
                 start_sample_idx = start_global_sample_idx - p1
-                print('find start')
-
             if start_part_idx != -1:
                 if p1 <= end_global_sample_idx < p2:
                     end_part_idx = j
                     end_tar_idx = k
                     end_sample_idx = end_global_sample_idx - p1
-                    print('find end')
 
             p1 = p2
 
-    return (start_part_idx, start_tar_idx, start_sample_idx), (end_part_idx, end_tar_idx, end_sample_idx)
+    #
+    tars_to_sync = {}
+    for j in range(start_part_idx, end_part_idx+1):
+        if start_part_idx == end_part_idx:
+            if j == start_part_idx:
+                tars_to_sync[j] = list(range(start_tar_idx, end_tar_idx+1))
+        else:
+            if j == start_part_idx:
+                tars_to_sync[j] = list(range(start_tar_idx, max_tars))
+            elif j == end_part_idx:
+                tars_to_sync[j] = list(range(0, end_tar_idx+1))
+            else:
+                tars_to_sync[j] = list(range(0, max_tars))
+    print("\r\nDevice id: ", device_id)
+    print("Split result:\nStart: ", start_part_idx, start_tar_idx, start_sample_idx)
+    print("End: ", end_part_idx, end_tar_idx, end_sample_idx)
+    print('tars to sync: ', tars_to_sync)
+    #alloc = [(0, 0, 0, 0, 0, 0, 0) for _ in range(num_devices)]
+
+    return (start_part_idx, start_tar_idx, start_sample_idx), (end_part_idx, end_tar_idx, end_sample_idx), tars_to_sync
 
 
 if __name__ == '__main__':
     num_parts = 2 #14
     max_tars = 3 # 533
-    num_devices = 2
+    num_devices = 4
 
     device_id = 1
 
@@ -57,28 +72,10 @@ if __name__ == '__main__':
 
     id_2_idx = []
 
-    #
-    start, end = get_split(num_samples, num_devices, device_id)
-
-    start_part_idx, start_tar_idx, start_sample_idx = start
-    end_part_idx, end_tar_idx, end_sample_idx = end
-
-    tars_to_sync = {}
-    if start_part_idx == end_part_idx:
-        if j == start_part_idx:
-            tars_to_sync[j] = list(range(start_tar_idx, end_tar_idx+1))
-    else:
-        for j in range(start_part_idx, end_part_idx+1):
-            if j == start_part_idx:
-                tars_to_sync[j] = list(range(start_tar_idx, max_tars))
-            elif j == end_part_idx:
-                tars_to_sync[j] = list(range(0, end_tar_idx+1))
-            else:
-                tars_to_sync[j] = list(range(0, max_tars))
-    print("Split result:\nStart: ", start_part_idx, start_tar_idx, start_sample_idx)
-    print("End: ", end_part_idx, end_tar_idx, end_sample_idx)
-    print('tars to sync: ', tars_to_sync)
-    #alloc = [(0, 0, 0, 0, 0, 0, 0) for _ in range(num_devices)]
+    for device_id in range(num_devices):
+        start, end, tars_to_sync = get_split(num_samples, num_devices, device_id)
+        start_part_idx, start_tar_idx, start_sample_idx = start
+        end_part_idx, end_tar_idx, end_sample_idx = end
 
 
     # verify
