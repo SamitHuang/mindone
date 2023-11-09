c2ef3fe2bc3475dfa42084695495979d211c63cf
samithuang <285365963@qq.com>
2023-11-09 14:28:10 +0000

    image infer ok

--- examples/animate_diff/ldm/util.py
+++ examples/animate_diff/ldm/util.py
@@ -25,7 +25,7 @@ import mindspore as ms
 import mindspore.ops as ops
 from mindspore import load_checkpoint, load_param_into_net
 
-_logger = logging.getLogger(__name__)
+logger = _logger = logging.getLogger(__name__)
 
 
 def log_txt_as_img(wh, xc, size=10):
@@ -121,3 +121,63 @@ def load_pretrained_model(pretrained_ckpt, net):
         _logger.info("Params not load: {}".format(param_not_load))
     else:
         _logger.warning("Checkpoint file not exists!!!")
+
+
+def load_model_from_config(config, ckpt, use_lora=False, lora_rank=4, lora_fp16=True, lora_only_ckpt=None, lora_scale=1.0):
+    model = instantiate_from_config(config.model)
+
+    def _load_model(_model, ckpt_fp, verbose=True, filter=None):
+        if os.path.exists(ckpt_fp):
+            param_dict = ms.load_checkpoint(ckpt_fp)
+            if param_dict:
+                param_not_load, ckpt_not_load = model_utils.load_param_into_net_with_filter(
+                    _model, param_dict, filter=filter
+                )
+                if verbose:
+                    if len(param_not_load) > 0:
+                        logger.info(
+                            "Net params not loaded: {}".format([p for p in param_not_load if not p.startswith("adam")])
+                        )
+        else:
+            logger.info(f"!!!Error!!!: {ckpt_fp} doesn't exist")
+            raise FileNotFoundError(f"{ckpt_fp} doesn't exist")
+
+    if use_lora:
+        load_lora_only = True if lora_only_ckpt is not None else False
+        if not load_lora_only:
+            logger.info(f"Loading model from {ckpt}")
+            _load_model(model, ckpt)
+        else:
+            if os.path.exists(lora_only_ckpt):
+                lora_param_dict = ms.load_checkpoint(lora_only_ckpt)
+                if "lora_rank" in lora_param_dict.keys():
+                    lora_rank = int(lora_param_dict["lora_rank"].value())
+                    logger.info(f"Lora rank is set to {lora_rank} according to the found value in lora checkpoint.")
+                else:
+                    raise ValueError("Missing lora rank in ckpt dict")
+            else:
+                raise ValueError(f"{lora_only_ckpt} doesn't exist")
+            # load the main pretrained model
+            logger.info(f"Loading pretrained model from {ckpt}")
+            _load_model(model, ckpt, verbose=True, filter=ms.load_checkpoint(ckpt).keys())
+            # inject lora params
+            injected_attns, injected_trainable_params = inject_trainable_lora(
+                model,
+                rank=lora_rank,
+                use_fp16=(model.model.diffusion_model.dtype == ms.float16),
+                scale=scale,
+            )
+            # load fine-tuned lora params
+            logger.info(f"Loading LoRA params from {lora_only_ckpt}")
+            _load_model(model, lora_only_ckpt, verbose=True, filter=injected_trainable_params.keys())
+    else:
+        logger.info(f"Loading model from {ckpt}")
+        _load_model(model, ckpt)
+
+    model.set_train(False)
+    for param in model.trainable_params():
+        param.requires_grad = False
+
+    return model
+
+
