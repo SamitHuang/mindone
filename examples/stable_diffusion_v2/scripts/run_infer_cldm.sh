f5cc8a785b74b45263cae74a5a250e44650eca90
samithuang <285365963@qq.com>
2023-11-17 10:13:50 +0800

    both id and ood pass

--- examples/stable_diffusion_v2/scripts/run_infer_cldm.sh
+++ examples/stable_diffusion_v2/scripts/run_infer_cldm.sh
@@ -1,7 +1,9 @@
 export DEVICE_ID=$1
 
-task_name=infer_cldm
+task_name=$3
 output_dir=output/$task_name
+
+scale=9
 
 cd inference
 mkdir -p $output_dir
@@ -12,6 +14,27 @@ python sd_infer.py \
 --model=./config/model/v1-inference-controlnet.yaml \
 --sampler=./config/schedule/ddim.yaml \
 --sampling_steps=50 \
+--scale=$scale \
+--n_iter=2 \
+--n_samples=4 \
+--controlnet_mode=canny \
+--control_path "../datasets/fill50k/source/2000.png" \
+--image_path "../datasets/fill50k/target/2000.png" \
+--prompt "white smoke circle with cornflower blue background" \
+--a_prompt "" \
+--negative_prompt "" \
+--pretrained_ckpt=../$2 \
+--output_path=$output_dir \
+
+
+
+python sd_infer.py \
+--device_target=Ascend \
+--task=controlnet \
+--model=./config/model/v1-inference-controlnet.yaml \
+--sampler=./config/schedule/ddim.yaml \
+--sampling_steps=50 \
+--scale=$scale \
 --n_iter=2 \
 --n_samples=4 \
 --controlnet_mode=canny \
@@ -23,7 +46,6 @@ python sd_infer.py \
 --pretrained_ckpt=../$2 \
 --output_path=$output_dir \
 
-
-echo "All results will be saved under ./inference folder"
+echo "All results will be saved under ./inference/${output_dir}"
 #--pretrained_ckpt=../output/train_cldm_canny_fill50k/ckpt/sd-16.ckpt \
 #--pretrained_ckpt="../models/control_sd15_canny_ms_static.ckpt" \
--- examples/stable_diffusion_v2/scripts/run_train_cldm.sh
+++ examples/stable_diffusion_v2/scripts/run_train_cldm.sh
@@ -2,7 +2,7 @@ export DEVICE_ID=$1
 export MS_ASCEND_CHECK_OVERFLOW_MODE="INFNAN_MODE" # debug
 # export MS_ASCEND_CHECK_OVERFLOW_MODE=1 # debug
 
-task_name=train_cldm_canny_fill1k_bs2_lr5e-4
+task_name=train_cldm_canny_fill1k_e14_constant_ema
 output_dir=outputs/$task_name
 
 rm -rf $output_dir
