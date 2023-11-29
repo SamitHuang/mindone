49bc4882fe3f43c8f38610f6adf314c04b7ad7f4
root <root@localhost.localdomain>
2023-11-29 18:43:11 +0800

    add script

---
+++ examples/stable_diffusion_xl/run_infer_lora.sh
@@ -0,0 +1,10 @@
+export DEVICE_ID=$1
+export MS_PYNATIVE_GE=1
+ckpt_path="outputs/train_lora_r4_InfNan/2023.11.29-17.26.19/weights/SDXL-base-1.0_2500_lora.ckpt"
+
+python demo/sampling_without_streamlit.py \
+  --task txt2img \
+  --config configs/training/sd_xl_base_finetune_lora_910b.yaml \
+  --weight $ckpt_path \
+  --prompt "a painting of a tree with a mountain in the background and a person standing in the foreground with a snow covered ground" \
+  --device_target Ascend
