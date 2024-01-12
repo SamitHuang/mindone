1e78efb5eb80f45548aa35013c33a21a8dd8afaf
samithuang <285365963@qq.com>
2024-01-12 10:45:49 +0800

    mmv2 webvid5 overfit ok

--- examples/animatediff/run_mmv2_train.sh
+++ examples/animatediff/run_mmv2_train.sh
@@ -1,8 +1,8 @@
 export MS_ASCEND_CHECK_OVERFLOW_MODE="INFNAN_MODE"
 # For best performance, set to a SD1.5 checkpoint trained from image-finetuning in 256x256 resolution 
 # pretrained_model_path=models/stable_diffusion/sd_v1.5-d0ab7146.ckpt
-pretrained_model_path=tmp_outputs/sdgen_overfit/2024-01-09T18-40-59/ckpt/sd-1000.ckpt 
+# pretrained_model_path=tmp_outputs/sdgen_overfit/2024-01-09T18-40-59/ckpt/sd-1000.ckpt 
 
-python train.py --config configs/training/mmv2_train.yaml \
-        --pretrained_model_path=$pretrained_model_path \
-        --output_path "outputs/mmv2_train_sdgen_fixPE_lr1e-4"
+python train.py --config configs/training/mmv2_train.yaml
+
+# --pretrained_model_path=$pretrained_model_path \
