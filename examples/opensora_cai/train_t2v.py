89903c01808d065872fd37b94fea6db21da95aff
wtomin <dengdidanwtomin@gmail.com>
2024-04-23 11:18:05 +0800

    skip loading vae when using cache

--- examples/opensora_cai/train_t2v.py
+++ examples/opensora_cai/train_t2v.py
@@ -174,10 +174,13 @@ def main(args):
     latte_model.set_train(True)
 
     # 2.2 vae
-    # TODO: use mindone/models/autoencoders in future
-    logger.info("vae init")
     train_with_vae_latent = args.vae_latent_folder is not None and os.path.exists(args.vae_latent_folder)
-    if not train_with_vae_latent:
+    if train_with_vae_latent:
+        logger.info("Train with vae latent cache.")
+        vae = None
+    else:
+        # TODO: use mindone/models/autoencoders in future
+        logger.info("vae init")
         vae = AutoencoderKL(
             SD_CONFIG,
             VAE_Z_CH,
@@ -187,11 +190,8 @@ def main(args):
         vae = vae.set_train(False)
         for param in vae.get_parameters():
             param.requires_grad = False
-    else:
-        vae = None
 
     # 2.3 ldm with loss
-    logger.info(f"Train with vae latent cache: {train_with_vae_latent}")
     diffusion = create_diffusion(timestep_respacing="")
     latent_diffusion_with_loss = DiffusionWithLoss(
         latte_model,
