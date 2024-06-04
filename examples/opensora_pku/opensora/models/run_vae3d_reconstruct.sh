99f161ffc9319d042926d18bb59bb0783d8ed669
samithuang <285365963@qq.com>
2024-06-04 17:41:12 +0800

    add reconstruct script (temp for test)

---
+++ examples/opensora_pku/opensora/models/run_vae3d_reconstruct.sh
@@ -0,0 +1,9 @@
+python test_reconstruct.py \
+    --ckpt_path /home/mindocr/yx/mindone/examples/opensora_pku/models/v1.1/vae/causal_vae_3d_v2.ckpt \
+    --dataset_name video \
+    --size 512 \
+    --crop_size 256 \
+    --num_frames 33 \
+    --data_path /home/mindocr/yx/mindone/examples/videocomposer/datasets/webvid5 \
+    --device_target GPU \
+
