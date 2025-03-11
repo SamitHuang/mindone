6bc2c87f6daada6f10860839b7490964edc49c10
samithuang <285365963@qq.com>
2025-03-11 11:09:35 +0800

    update test

--- examples/hunyuanvideo/tests/test_llava.py
+++ examples/hunyuanvideo/tests/test_llava.py
@@ -24,13 +24,16 @@ from transformers import AutoProcessor
 from mindone.transformers.models.llava import LlavaForConditionalGeneration
 from mindone.transformers.models.llava.configuration_llava import LlavaConfig
 
+from hyvideo.utils.helpers import set_model_param_dtype
+
 
 def test():
     model_path = "ckpts/llava-llama-3-8b-v1_1-transformers"
+    dtype = ms.float16
+
     # model_path = 'ckpts/llava_tiny'
     processor = AutoProcessor.from_pretrained(model_path)
 
-    # run
     prompt = (
         "<|start_header_id|>user<|end_header_id|>\n\n<image>\nWhat are these?<|eot_id|>"
         "<|start_header_id|>assistant<|end_header_id|>\n\n"
@@ -38,19 +41,21 @@ def test():
     image_file = "./000000039769.jpg"
     raw_image = Image.open(image_file)
 
-    inputs = processor(text=prompt, images=raw_image, return_tensors="np")  # .to(ms.float16)
+    inputs = processor(text=prompt, images=raw_image, return_tensors="np")
     inputs["input_ids"] = ms.tensor(inputs["input_ids"], dtype=ms.int32)
     inputs["attention_mask"] = ms.tensor(inputs["attention_mask"], dtype=ms.bool_)
-    inputs["pixel_values"] = ms.tensor(inputs["pixel_values"]).to(ms.float16)
+    inputs["pixel_values"] = ms.tensor(inputs["pixel_values"]).to(dtype)
 
-    # inputs .to(ms.float16)
 
-    # FIXME: support mixed precision setting, to(float16) may not be what we want
-    feature_only = True
+    feature_only = False
+    config = LlavaConfig.from_pretrained(model_path)
+    config.text_config._attn_implementation = "flash_attention_2"
+    model = LlavaForConditionalGeneration.from_pretrained(model_path, text_config=config.text_config)
+
+    if dtype != ms.float32:
+        set_model_param_dtype(model, dtype=dtype)
+
     if feature_only:
-        config = LlavaConfig.from_pretrained(model_path, mindspore_dtype=ms.float16)
-        config.text_config._attn_implementation = "flash_attention_2"
-        model = LlavaForConditionalGeneration.from_pretrained(model_path, text_config=config.text_config)
         outputs = model(
             input_ids=inputs["input_ids"],
             pixel_values=inputs["pixel_values"],
@@ -61,16 +66,10 @@ def test():
         )
         print("num hidden state: ", len(outputs.hidden_states[-1]))
         print("last hidden state: ", outputs.hidden_states[-1].shape)
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
     else:
-        config = LlavaConfig.from_pretrained(model_path, mindspore_dtype=ms.float16)
+        output = model.generate(**inputs, max_new_tokens=200 + 824, do_sample=False, use_cache=False)
 
-        # FIXME: fail to set llama FA
-        model = LlavaForConditionalGeneration.from_pretrained(
-            model_path, use_flash_attention_2=True, mindspore_dtype=ms.float16
-        )
-
-        output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
         print(processor.decode(output[0][2:], skip_special_tokens=True))
 
 
